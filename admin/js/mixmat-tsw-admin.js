3f2bc7a95c995e28ee3b4bb89c5289e6816c0281
Larry <tradesouthwest@outlook.com>
2023-02-21 15:20:40 -0700

    added fake break

--- admin/js/mixmat-tsw-admin.js
+++ admin/js/mixmat-tsw-admin.js
@@ -1,12 +1,6 @@
 (function( $ ) {
 	'use strict';
-/*
-	$(document).ready(function($) {
-        $('#mixmat_color').wpColorPicker();
-        $('#mixmat_shado').wpColorPicker();
-	});
-*/
-jQuery( document ).ready(function($) {
+$( document ).ready(function($) {
 
     tinymce.PluginManager.add( 'column', function( editor, url ) {
         // Add Button to Visual Editor Toolbar
@@ -16,22 +10,23 @@ jQuery( document ).ready(function($) {
             image: url + '/mxmtic-1.png',
         });
         editor.addCommand('column', function() {
-            var selected_text = 'content text sample'; /*editor.selection.getContent({
+            var selected_text = '<p>content text sample</p>'; /*editor.selection.getContent({
                 'format': 'html'
             });*/
             if ( selected_text.length === 0 ) {
                 alert( 'Please select some text.' );
                 return;
             }
-            var open_column = '<div class="mixmat_one">';
-            var close_column = '</div>';
+            var open_column = '<div class="mixmat-row"><div class="mixmat_one">';
+            var close_column = '</div></div><br class="mxmt-hr">';
             
             var return_text = '';
             return_text = open_column + selected_text + close_column;
-            editor.execCommand('mceReplaceContent', false, return_text);
+            editor.execCommand('mceInsertContent', false, return_text);
             return;
         });
     });
+
     tinymce.PluginManager.add( 'columns', function( editor, url ) {
         // Add Button to Visual Editor Toolbar
         editor.addButton('columns', {
@@ -39,27 +34,28 @@ jQuery( document ).ready(function($) {
             cmd: 'columns',
             image: url + '/mxmtic-2.png',
         });
-
         editor.addCommand('columns', function() {
-            var selected_text = 'content text sample'; /*editor.selection.getContent({
+        
+            var selected_text = editor.selection.getContent({
                 'format': 'html'
-            });*/
+            }) + 'sample content';
             if ( selected_text.length === 0 ) {
-                alert( 'Please select some text.' );
+                alert( 'Please set some text.' );
                 return;
             }
-            var open_column = '<div class="mixmat_one_half">';
+            var open_column = '<div class="mixmat-row"><div class="mixmat_one_half">';
             var close_column = '</div>';
             var open_culumn = '<div class="mixmat_last_one_half">';
-            var close_culumn = '</div>';
+            var close_culumn = '</div></div><br class="mxmt-hr">';
             
 			var return_text = '';
             return_text = open_column + selected_text + close_column
 						+ open_culumn + selected_text + close_culumn;
-            editor.execCommand('mceReplaceContent', false, return_text);
+            editor.execCommand('mceInsertContent', false, return_text);
             return;
         });
-	});
+    });
+
     tinymce.PluginManager.add( 'columnsThree', function( editor, url ) {
         // Add Button to Visual Editor Toolbar
         editor.addButton('columnsThree', {
@@ -67,31 +63,30 @@ jQuery( document ).ready(function($) {
             cmd: 'columnsThree',
             image: url + '/mxmtic-3.png',
         });
-	
-
         editor.addCommand('columnsThree', function() {
-            var selected_text = 'content text sample'; /*editor.selection.getContent({
+            var selected_text = '<p>content text sample</p>'; /*editor.selection.getContent({
                 'format': 'html'
             });*/
             if ( selected_text.length === 0 ) {
                 alert( 'Please select some text.' );
                 return;
             }
-            var open_column = '<div class="mixmat_one_third">';
+            var open_column = '<div class="mixmat-row"><div class="mixmat_one_third">';
             var close_column = '</div>';
             var open_columnb = '<div class="mixmat_one_third">';
             var close_columnb = '</div>';
             var open_culumn = '<div class="mixmat_last_one_third">';
-            var close_culumn = '</div>';
+            var close_culumn = '</div></div><br class="mxmt-hr">';
             
 			var return_text = '';
             return_text = open_column + selected_text + close_column
                         + open_columnb + selected_text + close_columnb
 						+ open_culumn + selected_text + close_culumn;
-            editor.execCommand('mceReplaceContent', false, return_text);
+            editor.execCommand('mceInsertContent', false, return_text);
             return;
         });
 	});
+
     tinymce.PluginManager.add( 'columnsFour', function( editor, url ) {
         // Add Button to Visual Editor Toolbar
         editor.addButton('columnsFour', {
@@ -99,33 +94,34 @@ jQuery( document ).ready(function($) {
             cmd: 'columns4',
             image: url + '/mxmtic-4.png',
         });
-	
-
         editor.addCommand('columnsFour', function() {
-            var selected_text = 'content text sample'; /*editor.selection.getContent({
+            var selected_text = '<p>content text sample</p>'; /*editor.selection.getContent({
                 'format': 'html'
             });*/
             if ( selected_text.length === 0 ) {
                 alert( 'Please select some text.' );
                 return;
             }
-            var open_column = '<div class="mixmat_one_fourth">';
+            var open_column = '<div class="mixmat-row"><div class="mixmat_one_fourth">';
             var close_column = '</div>';
             var open_columnb = '<div class="mixmat_one_fourth">';
             var close_columnb = '</div>';
             var open_columnc = '<div class="mixmat_one_fourth">';
             var close_columnc = '</div>';
             var open_culumn = '<div class="mixmat_last_one_fourth">';
-            var close_culumn = '</div>';
+            var close_culumn = '</div></div><br class="mxmt-hr">';
             
 			var return_text = '';
             return_text = open_column + selected_text + close_column
                         + open_columnb + selected_text + close_columnb
                         + open_columnc + selected_text + close_columnc
 						+ open_culumn + selected_text + close_culumn;
-            editor.execCommand('mceReplaceContent', false, return_text);
+            editor.execCommand('mceInsertContent', false, return_text);
             return;
         });
 	});
+
 });
+
+
 })( jQuery );
